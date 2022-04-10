b4eea27ccd2ac651c992bcc1f59169425c16f9c3
unknown <xarturino>
2022-04-10 10:28:19 +0300

    lesson2 homework, first task - done

---
+++ src/main/java/lesson2/SumOfTwoNumbersMethod.java
@@ -0,0 +1,20 @@
+package lesson2;
+
+public class SumOfTwoNumbersMethod {
+    public static void main(String[] args) {
+        sumOfTwoNumbers(15,18);
+    }
+
+    static boolean sumOfTwoNumbers(int firstNumber, int secondNumber){
+        int a = firstNumber;
+        int b = secondNumber;
+        if (10 < (a+b) && (a+b) <=20){
+            System.out.println(true);
+            return true;
+        }
+        else {
+            System.out.println(false);
+            return false;
+        }
+    }
+}

5756c96025f2d8a3165d26716851bc1aa5c90b92
unknown <xarturino>
2022-04-10 23:21:39 +0300

    lesson2 homework, all numeral tasks are done, two 'star' task are done. TODO: second part of task 8 and third 'star task'

--- src/main/java/lesson2/PaintOverTheDiagonals.java
+++ src/main/java/lesson2/PaintOverTheDiagonals.java
@@ -4,11 +4,28 @@ public class PaintOverTheDiagonals {
 
     public static void main(String[] args) {
 
-        //TODO: paint second diagonal
+        //TODO: paint second diagonal +
+        // Done, but don't like solution.
         int[][] squareArray= new int[5][5];
         for (int i = 0; i < squareArray.length; i++){
             for (int z = 0; z < squareArray.length; z++){
                 if (i == z){
+                    squareArray[i][z] = 1;
+                    System.out.print(" " + squareArray[i][z] + " ");
+                }
+                else if(i == 4 && z == 0){
+                    squareArray[i][z] = 1;
+                    System.out.print(" " + squareArray[i][z] + " ");
+                }
+                else if(i == 3 && z == 1){
+                    squareArray[i][z] = 1;
+                    System.out.print(" " + squareArray[i][z] + " ");
+                }
+                else if(z == 4 && i == 0){
+                    squareArray[i][z] = 1;
+                    System.out.print(" " + squareArray[i][z] + " ");
+                }
+                else if(z == 3 && i == 1){
                     squareArray[i][z] = 1;
                     System.out.print(" " + squareArray[i][z] + " ");
                 }
